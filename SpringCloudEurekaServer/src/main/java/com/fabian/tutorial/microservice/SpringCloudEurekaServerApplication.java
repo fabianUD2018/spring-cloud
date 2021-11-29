750faf27b3f40a3150c1f5223e1005126321c4b6
fabianUD2018 <38334937+fabianUD2018@users.noreply.github.com>
2021-11-29 14:27:32 -0500

    scalar microservcios con puerto dinámico

--- SpringCloudEurekaServer/src/main/java/com/fabian/tutorial/microservice/SpringCloudEurekaServerApplication.java
+++ SpringCloudEurekaServer/src/main/java/com/fabian/tutorial/microservice/SpringCloudEurekaServerApplication.java
@@ -2,14 +2,14 @@ package com.fabian.tutorial.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaClient
+@EnableEurekaServer
 @SpringBootApplication
-public class SpringCloudServiceApplication {
+public class SpringCloudEurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringCloudServiceApplication.class, args);
+		SpringApplication.run(SpringCloudEurekaServerApplication.class, args);
 	}
 
 }
